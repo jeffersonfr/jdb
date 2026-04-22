caa8d01f7010c0efb355aad107d11a1a73909333
Jefferson Ferreira <jefferson.ferreira@egsys.com.br>
2026-04-21 22:04:55 -0300

    Adjusting extended model to accept only pure data

--- include/jdb/database/ExtendedModel.hpp
+++ include/jdb/database/ExtendedModel.hpp
@@ -3,11 +3,159 @@
 #include "jdb/database/DataClass.hpp"
 
 namespace jdb {
-  template<typename Model, FieldConcept... Fields>
+  template<typename Model, typename... Fields>
     struct ExtendedModel;
 
   template<jmixin::StringLiteral Name, PrimaryConcept PrimaryKeys, ForeignConcept ForeignKeys, FieldConcept... Fields, FieldConcept ...NewFields>
   struct ExtendedModel<DataClass<Name, PrimaryKeys, ForeignKeys, Fields...>, NewFields...> : DataClass<Name, PrimaryKeys, ForeignKeys, Fields..., NewFields...> {
       ~ExtendedModel() override = default;
     };
+
+  /*
+  template<jmixin::StringLiteral Name, PrimaryConcept PrimaryKeys, ForeignConcept ForeignKeys, FieldConcept... Fields, jmixin::StringLiteral Name2, PrimaryConcept PrimaryKeys2, ForeignConcept ForeignKeys2, FieldConcept ...NewFields2>
+  struct ExtendedModel<DataClass<Name, PrimaryKeys, ForeignKeys, Fields...>, DataClass<Name2, PrimaryKeys2, ForeignKeys2, NewFields2...>> : DataClass<Name, PrimaryKeys, ForeignKeys, Fields..., NewFields2...> {
+    ~ExtendedModel() override = default;
+  };
+
+  template<jmixin::StringLiteral Name, PrimaryConcept PrimaryKeys, ForeignConcept ForeignKeys, FieldConcept... Fields, jmixin::StringLiteral ...NamesExt, FieldConcept ...NewFieldsExt>
+  struct ExtendedModel<DataClass<Name, PrimaryKeys, ForeignKeys, Fields...>, DataClass<NamesExt, NoPrimary, NoForeign, NewFieldsExt>...> : DataClass<Name, PrimaryKeys, ForeignKeys, Fields..., NewFieldsExt...> {
+    ~ExtendedModel() override = default;
+  };
+  */
+
+  template<
+    jmixin::StringLiteral Name0, PrimaryConcept PrimaryKeys0, ForeignConcept ForeignKeys0, FieldConcept... Fields0,
+    jmixin::StringLiteral Name1, FieldConcept ...Fields1>
+  struct ExtendedModel<
+    DataClass<Name0, PrimaryKeys0, ForeignKeys0, Fields0...>,
+    DataClass<Name1, NoPrimary, NoForeign, Fields1...>
+  > : DataClass<Name0, PrimaryKeys0, ForeignKeys0, Fields0..., Fields1...> {
+    ~ExtendedModel() override = default;
+  };
+
+  template<
+    jmixin::StringLiteral Name0, PrimaryConcept PrimaryKeys0, ForeignConcept ForeignKeys0, FieldConcept... Fields0,
+    jmixin::StringLiteral Name1, FieldConcept ...Fields1,
+    jmixin::StringLiteral Name2, FieldConcept ...Fields2>
+  struct ExtendedModel<
+    DataClass<Name0, PrimaryKeys0, ForeignKeys0, Fields0...>,
+    DataClass<Name1, NoPrimary, NoForeign, Fields1...>,
+    DataClass<Name2, NoPrimary, NoForeign, Fields2...>
+  > : DataClass<Name0, PrimaryKeys0, ForeignKeys0, Fields0..., Fields1..., Fields2...> {
+    ~ExtendedModel() override = default;
+  };
+
+  template<
+    jmixin::StringLiteral Name0, PrimaryConcept PrimaryKeys0, ForeignConcept ForeignKeys0, FieldConcept... Fields0,
+    jmixin::StringLiteral Name1, FieldConcept ...Fields1,
+    jmixin::StringLiteral Name2, FieldConcept ...Fields2,
+    jmixin::StringLiteral Name3, FieldConcept ...Fields3>
+  struct ExtendedModel<
+    DataClass<Name0, PrimaryKeys0, ForeignKeys0, Fields0...>,
+    DataClass<Name1, NoPrimary, NoForeign, Fields1...>,
+    DataClass<Name2, NoPrimary, NoForeign, Fields2...>,
+    DataClass<Name3, NoPrimary, NoForeign, Fields3...>
+  > : DataClass<Name0, PrimaryKeys0, ForeignKeys0, Fields0..., Fields1..., Fields2..., Fields3...> {
+    ~ExtendedModel() override = default;
+  };
+
+  template<
+    jmixin::StringLiteral Name0, PrimaryConcept PrimaryKeys0, ForeignConcept ForeignKeys0, FieldConcept... Fields0,
+    jmixin::StringLiteral Name1, FieldConcept ...Fields1,
+    jmixin::StringLiteral Name2, FieldConcept ...Fields2,
+    jmixin::StringLiteral Name3, FieldConcept ...Fields3,
+    jmixin::StringLiteral Name4, FieldConcept ...Fields4>
+  struct ExtendedModel<
+    DataClass<Name0, PrimaryKeys0, ForeignKeys0, Fields0...>,
+    DataClass<Name1, NoPrimary, NoForeign, Fields1...>,
+    DataClass<Name2, NoPrimary, NoForeign, Fields2...>,
+    DataClass<Name3, NoPrimary, NoForeign, Fields3...>,
+    DataClass<Name4, NoPrimary, NoForeign, Fields4...>
+  > : DataClass<Name0, PrimaryKeys0, ForeignKeys0, Fields0..., Fields1..., Fields2..., Fields3..., Fields4...> {
+    ~ExtendedModel() override = default;
+  };
+
+  template<
+    jmixin::StringLiteral Name0, PrimaryConcept PrimaryKeys0, ForeignConcept ForeignKeys0, FieldConcept... Fields0,
+    jmixin::StringLiteral Name1, FieldConcept ...Fields1,
+    jmixin::StringLiteral Name2, FieldConcept ...Fields2,
+    jmixin::StringLiteral Name3, FieldConcept ...Fields3,
+    jmixin::StringLiteral Name4, FieldConcept ...Fields4,
+    jmixin::StringLiteral Name5, FieldConcept ...Fields5>
+  struct ExtendedModel<
+    DataClass<Name0, PrimaryKeys0, ForeignKeys0, Fields0...>,
+    DataClass<Name1, NoPrimary, NoForeign, Fields1...>,
+    DataClass<Name2, NoPrimary, NoForeign, Fields2...>,
+    DataClass<Name3, NoPrimary, NoForeign, Fields3...>,
+    DataClass<Name4, NoPrimary, NoForeign, Fields4...>,
+    DataClass<Name5, NoPrimary, NoForeign, Fields5...>
+  > : DataClass<Name0, PrimaryKeys0, ForeignKeys0, Fields0..., Fields1..., Fields2..., Fields3..., Fields4..., Fields5...> {
+    ~ExtendedModel() override = default;
+  };
+
+  template<
+    jmixin::StringLiteral Name0, PrimaryConcept PrimaryKeys0, ForeignConcept ForeignKeys0, FieldConcept... Fields0,
+    jmixin::StringLiteral Name1, FieldConcept ...Fields1,
+    jmixin::StringLiteral Name2, FieldConcept ...Fields2,
+    jmixin::StringLiteral Name3, FieldConcept ...Fields3,
+    jmixin::StringLiteral Name4, FieldConcept ...Fields4,
+    jmixin::StringLiteral Name5, FieldConcept ...Fields5,
+    jmixin::StringLiteral Name6, FieldConcept ...Fields6>
+  struct ExtendedModel<
+    DataClass<Name0, PrimaryKeys0, ForeignKeys0, Fields0...>,
+    DataClass<Name1, NoPrimary, NoForeign, Fields1...>,
+    DataClass<Name2, NoPrimary, NoForeign, Fields2...>,
+    DataClass<Name3, NoPrimary, NoForeign, Fields3...>,
+    DataClass<Name4, NoPrimary, NoForeign, Fields4...>,
+    DataClass<Name5, NoPrimary, NoForeign, Fields5...>,
+    DataClass<Name6, NoPrimary, NoForeign, Fields6...>
+  > : DataClass<Name0, PrimaryKeys0, ForeignKeys0, Fields0..., Fields1..., Fields2..., Fields3..., Fields4..., Fields5..., Fields6...> {
+    ~ExtendedModel() override = default;
+  };
+
+  template<
+    jmixin::StringLiteral Name0, PrimaryConcept PrimaryKeys0, ForeignConcept ForeignKeys0, FieldConcept... Fields0,
+    jmixin::StringLiteral Name1, FieldConcept ...Fields1,
+    jmixin::StringLiteral Name2, FieldConcept ...Fields2,
+    jmixin::StringLiteral Name3, FieldConcept ...Fields3,
+    jmixin::StringLiteral Name4, FieldConcept ...Fields4,
+    jmixin::StringLiteral Name5, FieldConcept ...Fields5,
+    jmixin::StringLiteral Name6, FieldConcept ...Fields6,
+    jmixin::StringLiteral Name7, FieldConcept ...Fields7>
+  struct ExtendedModel<
+    DataClass<Name0, PrimaryKeys0, ForeignKeys0, Fields0...>,
+    DataClass<Name1, NoPrimary, NoForeign, Fields1...>,
+    DataClass<Name2, NoPrimary, NoForeign, Fields2...>,
+    DataClass<Name3, NoPrimary, NoForeign, Fields3...>,
+    DataClass<Name4, NoPrimary, NoForeign, Fields4...>,
+    DataClass<Name5, NoPrimary, NoForeign, Fields5...>,
+    DataClass<Name6, NoPrimary, NoForeign, Fields6...>,
+    DataClass<Name7, NoPrimary, NoForeign, Fields7...>
+  > : DataClass<Name0, PrimaryKeys0, ForeignKeys0, Fields0..., Fields1..., Fields2..., Fields3..., Fields4..., Fields5..., Fields6..., Fields7...> {
+    ~ExtendedModel() override = default;
+  };
+
+  template<
+    jmixin::StringLiteral Name0, PrimaryConcept PrimaryKeys0, ForeignConcept ForeignKeys0, FieldConcept... Fields0,
+    jmixin::StringLiteral Name1, FieldConcept ...Fields1,
+    jmixin::StringLiteral Name2, FieldConcept ...Fields2,
+    jmixin::StringLiteral Name3, FieldConcept ...Fields3,
+    jmixin::StringLiteral Name4, FieldConcept ...Fields4,
+    jmixin::StringLiteral Name5, FieldConcept ...Fields5,
+    jmixin::StringLiteral Name6, FieldConcept ...Fields6,
+    jmixin::StringLiteral Name7, FieldConcept ...Fields7,
+    jmixin::StringLiteral Name8, FieldConcept ...Fields8>
+  struct ExtendedModel<
+    DataClass<Name0, PrimaryKeys0, ForeignKeys0, Fields0...>,
+    DataClass<Name1, NoPrimary, NoForeign, Fields1...>,
+    DataClass<Name2, NoPrimary, NoForeign, Fields2...>,
+    DataClass<Name3, NoPrimary, NoForeign, Fields3...>,
+    DataClass<Name4, NoPrimary, NoForeign, Fields4...>,
+    DataClass<Name5, NoPrimary, NoForeign, Fields5...>,
+    DataClass<Name6, NoPrimary, NoForeign, Fields6...>,
+    DataClass<Name7, NoPrimary, NoForeign, Fields7...>,
+    DataClass<Name8, NoPrimary, NoForeign, Fields8...>
+  > : DataClass<Name0, PrimaryKeys0, ForeignKeys0, Fields0..., Fields1..., Fields2..., Fields3..., Fields4..., Fields5..., Fields6..., Fields7..., Fields8...> {
+    ~ExtendedModel() override = default;
+  };
 }
--- tests/db_unit_test.cpp
+++ tests/db_unit_test.cpp
@@ -15,11 +15,15 @@ using UserModel = DataClass<"user", Primary<"id">, NoForeign,
   Field<"id", FieldType::Serial, false>,
   Field<"name", FieldType::Int, false>,
   Field<"address", FieldType::Int, false>,
-  Field<"description", FieldType::Text, false>>;
+  Field<"description", FieldType::Text, false> >;
 
 using LoginModel = DataClass<"login", Primary<"id">, NoForeign,
   Field<"user_id", FieldType::Serial, false>,
-  ConstrainedField<1, "pass", FieldType::Int, false>>;
+  ConstrainedField<1, "pass", FieldType::Int, false> >;
+
+using SimpleModel = DataClass<"simple", NoPrimary, NoForeign,
+  Field<"simple_id", FieldType::Serial, false>,
+  Field<"simple_data", FieldType::Text, false>>;
 
 using CompoundUser = CompoundModel<UserModel, LoginModel>;
 
@@ -30,10 +34,13 @@ using ExtendedUserModel = ExtendedModel<UserModel,
   Field<"new_field", FieldType::Text, false>
 >;
 
+using ExtendedUserSimpleModel = ExtendedModel<UserModel, SimpleModel>;
+
 struct Environment : public ::testing::Environment {
   Environment() = default;
 
   void SetUp() override {
+    auto model = ExtendedUserSimpleModel{};
   }
 
   void TearDown() override {
@@ -66,11 +73,11 @@ TEST_F(jDbSuite, SimpleMigration) {
   auto db = std::make_shared<MyDatabase>("sistema.db");
 
   db->add_migration(Migration{
-    1, [](Database &db) {
-      // do nothing ...
-    }
-  })
-  .build();
+        1, [](Database &db) {
+          // do nothing ...
+        }
+      })
+      .build();
 }
 
 TEST_F(jDbSuite, SimpleConvertion) {
@@ -99,17 +106,21 @@ TEST_F(jDbSuite, DumpModel) {
   CompoundUser compound{user, login};
 
   // no constrained
-  ASSERT_EQ(user.to_string(), "{'id':1, 'name':'Jeff Ferr', 'address':'First District', 'description':'Some description'}");
+  ASSERT_EQ(user.to_string(),
+            "{'id':1, 'name':'Jeff Ferr', 'address':'First District', 'description':'Some description'}");
   ASSERT_EQ(login.to_string(), "{'user_id':1, 'pass':'12345678'}");
-  ASSERT_EQ(compound.to_string(), "{'user': {'id':1, 'name':'Jeff Ferr', 'address':'First District', 'description':'Some description'}, 'login': {'user_id':1, 'pass':'12345678'}}");
+  ASSERT_EQ(compound.to_string(),
+            "{'user': {'id':1, 'name':'Jeff Ferr', 'address':'First District', 'description':'Some description'}, 'login': {'user_id':1, 'pass':'12345678'}}");
 
   // constrained with RestrictLevel = 0
   ASSERT_EQ(login.restrict<0>().to_string(), "{'user_id':1}");
-  ASSERT_EQ(compound.restrict<0>().to_string(), "{'user': {'id':1, 'name':'Jeff Ferr', 'address':'First District', 'description':'Some description'}, 'login': {'user_id':1}}");
+  ASSERT_EQ(compound.restrict<0>().to_string(),
+            "{'user': {'id':1, 'name':'Jeff Ferr', 'address':'First District', 'description':'Some description'}, 'login': {'user_id':1}}");
 
   // constrained with RestrictLevel = 1
   ASSERT_EQ(login.restrict<1>().to_string(), "{'user_id':1, 'pass':'12345678'}");
-  ASSERT_EQ(compound.restrict<1>().to_string(), "{'user': {'id':1, 'name':'Jeff Ferr', 'address':'First District', 'description':'Some description'}, 'login': {'user_id':1, 'pass':'12345678'}}");
+  ASSERT_EQ(compound.restrict<1>().to_string(),
+            "{'user': {'id':1, 'name':'Jeff Ferr', 'address':'First District', 'description':'Some description'}, 'login': {'user_id':1, 'pass':'12345678'}}");
 }
 
 int main(int argc, char *argv[]) {
